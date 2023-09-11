71901bf64bb7e4ff72614f2e9a5edbfd27a10885
Juan Azambuja <juan@mimiquate.com>
2023-09-11 11:31:42 -0300

    wip working

--- tests/conformance_ruby/translate_test.rs
+++ tests/conformance_ruby/translate_test.rs
@@ -41,28 +41,30 @@ impl Filter for TranslateFilter {
     fn evaluate(&self, input: &dyn ValueView, runtime: &dyn Runtime) -> Result<Value> {
         println!("==================== HERE AT evalution");
         let path = input.to_kstr().to_string();
-        // let args = self.args.evaluate(runtime)?;
-        println!("{:?}", self.args);
+        let args = self.args.evaluate(runtime)?;
+        println!("peñarol");
+        println!("{:?}", args.variables);
 
         // We load translations from the runtime (considering them as global variables).
         let translations_config = runtime.get(&[KNOCK_TRANSLATIONS_VAR_NAME.into()]);
 
         let translation = match translations_config {
             Ok(parsed_translation_configs) => {
-
                 // translation configs will be converted to liquid objects when sending them as
                 // params through the Rust NIF bridge
                 match parsed_translation_configs.as_object() {
-                    Some(translations_config_object) => resolve_translation(path, translations_config_object),
-                    _ => return Ok(Value::scalar(""))
+                    Some(translations_config_object) => {
+                        resolve_translation(path, translations_config_object)
+                    }
+                    _ => return Ok(Value::scalar("")),
                 }
-            },
-            Err(_) => return Ok(Value::scalar(""))
+            }
+            Err(_) => return Ok(Value::scalar("")),
         };
 
         match translation {
             Ok(result) => Ok(Value::scalar(result)),
-            _ => Ok(Value::scalar(""))
+            _ => Ok(Value::scalar("")),
         }
     }
 }
@@ -72,26 +74,26 @@ fn resolve_translation(path: String, translations_config: &dyn ObjectView) -> Re
 
     match namespace_and_path[..] {
         [namespace, path_as_string] => {
-             let path_as_vector = path_as_string.split(KEY_SEPARATOR);
-             let namespace_key = format!("$_{}", namespace);
-             let namespace_translations = translations_config.get(&namespace_key);
-             let mut resolved_translation = match namespace_translations {
-                 Some(resolved_namespace_translations) => resolved_namespace_translations,
-                 _ => return Ok(String::from(""))
-             };
+            let path_as_vector = path_as_string.split(KEY_SEPARATOR);
+            let namespace_key = format!("$_{}", namespace);
+            let namespace_translations = translations_config.get(&namespace_key);
+            let mut resolved_translation = match namespace_translations {
+                Some(resolved_namespace_translations) => resolved_namespace_translations,
+                _ => return Ok(String::from("")),
+            };
 
-             for key in path_as_vector {
-                 resolved_translation = match resolved_translation.as_object().unwrap().get(key) {
-                     Some(value) => value,
-                     None => return Ok(String::from("")),
-                 };
-             }
+            for key in path_as_vector {
+                resolved_translation = match resolved_translation.as_object().unwrap().get(key) {
+                    Some(value) => value,
+                    None => return Ok(String::from("")),
+                };
+            }
 
-             match resolved_translation.as_scalar() {
-                 Some(result) => Ok(result.into_cow_str().to_string()),
-                 _ => Ok(String::from(""))
-             }
-        },
+            match resolved_translation.as_scalar() {
+                Some(result) => Ok(result.into_cow_str().to_string()),
+                _ => Ok(String::from("")),
+            }
+        }
         [path_as_string] => {
             let mut path_parts = path_as_string.split('.');
             let mut resolved_translation;
@@ -103,23 +105,23 @@ fn resolve_translation(path: String, translations_config: &dyn ObjectView) -> Re
                 };
 
                 for key in path_parts {
-                    resolved_translation = match resolved_translation.as_object().unwrap().get(key) {
+                    resolved_translation = match resolved_translation.as_object().unwrap().get(key)
+                    {
                         Some(value) => value,
                         None => return Ok(String::from("")),
                     };
                 }
             } else {
-                return Ok(String::from(""))
+                return Ok(String::from(""));
             }
 
             match resolved_translation.as_scalar() {
                 Some(result) => Ok(result.into_cow_str().to_string()),
-                _ => Ok(String::from(""))
+                _ => Ok(String::from("")),
             }
-        },
-        _ => Ok(String::from(""))
+        }
+        _ => Ok(String::from("")),
     }
-
 }
 
 fn liquid_with_translate() -> liquid::Parser {
@@ -131,12 +133,12 @@ fn liquid_with_translate() -> liquid::Parser {
 
 #[test]
 fn test_filter_translation() {
-    let assigns = o!({"first_name": "John"});
+    let assigns = o!({"first_name": "John", "knock_translations_config": o!({"layout": o!({"header": "Oh my header"})})});
     //  "HI {{user.first_name}}"
 
     assert_template_result!(
         "Hello John, literal!",
-        "{{ 'layout.header.hello_user' | t: name: first_name, other: 'literal' }}",
+        "{{ 'layout.hello_user' | t: name: first_name, other: 'literal' }}",
         assigns,
         liquid_with_translate()
     );
